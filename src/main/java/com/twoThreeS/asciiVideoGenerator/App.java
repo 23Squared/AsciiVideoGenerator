9b1e156fc9c2d339c0ffb3c980fb64075730c3d7
Rich J <rich@23squared.com>
2016-06-11 11:32:44 +0100

    Initial commit

---
+++ src/main/java/com/twoThreeS/asciiVideoGenerator/App.java
@@ -0,0 +1,19 @@
+package com.twoThreeS.asciiVideoGenerator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+
+
+    }
+}
